5d77ad0b3520889f5ed08f86f5f8c4118f8548f3
Viktor geozukunft <geozukunft@gmail.com>
2020-12-02 19:41:15 +0100

    init

--- MQTTRelay/Form1.Designer.cs
+++ MQTTRelay/Form1.Designer.cs
@@ -29,12 +29,110 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.consoleLog = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.serialCommToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cboPorts = new System.Windows.Forms.ToolStripComboBox();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // consoleLog
+            // 
+            this.consoleLog.Location = new System.Drawing.Point(36, 37);
+            this.consoleLog.Name = "consoleLog";
+            this.consoleLog.ReadOnly = true;
+            this.consoleLog.Size = new System.Drawing.Size(224, 20);
+            this.consoleLog.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serialCommToolStripMenuItem,
+            this.closeApplicationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // serialCommToolStripMenuItem
+            // 
+            this.serialCommToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.portToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.serialCommToolStripMenuItem.Name = "serialCommToolStripMenuItem";
+            this.serialCommToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.serialCommToolStripMenuItem.Text = "Serial Comm";
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cboPorts,
+            this.openToolStripMenuItem});
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.portToolStripMenuItem.Text = "Port";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 23);
+            this.cboPorts.Click += new System.EventHandler(this.cboPorts_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // closeApplicationToolStripMenuItem
+            // 
+            this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.closeApplicationToolStripMenuItem.Text = "Close Application";
+            this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplicationToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.consoleLog);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox consoleLog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem serialCommToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox cboPorts;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- MQTTRelay/Form1.cs
+++ MQTTRelay/Form1.cs
@@ -7,14 +7,132 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.IO;
+using System.IO.Ports;
 
 namespace MQTTRelay
 {
     public partial class Form1 : Form
     {
+        MqttClient client;
+        string clientID;
+        public delegate void SendDataDelegate(String myString);
+        public SendDataDelegate Delegate1;
+
+
         public Form1()
         {
             InitializeComponent();
+            string adress = "Serveradress";
+            string user = "username";
+            string pw = "SUPERSECUREPASSWORDNOTFORYOUREYES";
+
+            client = new MqttClient(adress, 8883, true, null, null, MqttSslProtocols.TLSv1_2);
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            client.Connect(Guid.NewGuid().ToString(), user, pw);
+
+
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+            this.Delegate1 = new SendDataDelegate(sendData);
+
+
+            client.Subscribe(new string[] { "car" }, new byte[] { 2 });
+
+
+
         }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            consoleLog.Invoke(this.Delegate1, new object[] { indata } );
+
+        }
+
+        public void sendData(String myString)
+        {
+            consoleLog.Text = myString;
+            //client.Publish("school/aiit", Encoding.UTF8.GetBytes(myString), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+            serialPort1.WriteLine(myString);
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(cboPorts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select COM-POrt");
+            }
+            else
+            {
+                serialPort1.PortName = cboPorts.SelectedItem.ToString();
+                if (!serialPort1.IsOpen)
+                {
+                    try
+                    {
+                        serialPort1.BaudRate = 9600;
+                        serialPort1.Open();
+                        MessageBox.Show("Port opened");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("A error occurred!");
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                MessageBox.Show("Port closed");
+            }
+            else
+            {
+                MessageBox.Show("Port not open");
+            }
+        }
+
+        private void closeApplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+            Application.Exit();
+        }
+
+        private void cboPorts_Click(object sender, EventArgs e)
+        {
+            ListCom();
+        }
+
+
+        private void ListCom()
+        {
+            //Write all available ports into String
+            string[] ports = SerialPort.GetPortNames();
+
+            //Display available Ports
+            foreach (string port in ports)
+            {
+                cboPorts.Items.Add(port);
+            }
+
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            consoleLog.Invoke(this.Delegate1, new object[] { ReceivedMessage });
+
+
+        }
+
     }
 }
